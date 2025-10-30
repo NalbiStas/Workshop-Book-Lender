b67c036a88e60d99eddcd6f4f9176592c91655a3
Nalbi Stas <stas.nalbi@gmail.com>
2025-10-30 16:23:13 +0100

    Created a constructor to initialize and set variables for storing book data.

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,5 +1,20 @@
 package se.lexicon.model;
 
 public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+        this.borrower = null;
+    }
+
     // todo: needs completion
 }
