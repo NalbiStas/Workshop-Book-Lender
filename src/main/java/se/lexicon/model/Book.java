b5872ba7fb034ce3a1da728a466b750c5ed2e622
Nalbi Stas <stas.nalbi@gmail.com>
2025-11-01 19:03:18 +0100

    Added getters, setters, and methods to manage borrower and book availability

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -16,5 +16,29 @@ public class Book {
         this.borrower = null;
     }
 
+    public String getId() { return this.id; }
+    public String getTitle() { return this.title; }
+    public String getAuthor() { return this.author; }
+    public boolean isAvailable() { return this.available; }
+    public Person getBorrower() { return this.borrower; }
+
+
+    public void setId(String id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public void setBorrower(Person borrower) {
+        this.borrower = borrower;
+        this.available = (borrower == null); //
+    }
+
+
+    public String getBookInformation() {
+        String status = available
+                ? "Available"
+                : "Borrowed by " + borrower.getPersonInformation();
+        return "Book ID: " + id + ", Title: " + title + ", Author: " + author + ", " + status;
+    }
+
     // todo: needs completion
 }
