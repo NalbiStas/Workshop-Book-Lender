8d98dd455d5468adc17159004dad0afa78f59c36
Nalbi Stas <stas.nalbi@gmail.com>
2025-11-02 16:27:46 +0100

    Added Person class fields for id generation, personal details, and borrowed book tracking

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,5 +1,13 @@
 package se.lexicon.model;
 
 public class Person {
+
+    private static int sequencer = 0;
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Book[] borrowedBooks;
+    private int bookCount;
     // todo: needs completion
 }
